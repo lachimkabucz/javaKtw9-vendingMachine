a988682a08398aeebab439bbc9af2eee9cce81ce
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-11-03 12:03:42 +0100

    Configuration by properties file

--- src/main/java/pl/sdacademy/vending/model/VendingMachine.java
+++ src/main/java/pl/sdacademy/vending/model/VendingMachine.java
@@ -1,11 +1,27 @@
 package pl.sdacademy.vending.model;
 
+import pl.sdacademy.vending.util.Configuration;
+
 public class VendingMachine {
+
+    private final Configuration configuration;
+
+    public VendingMachine() {
+        configuration = new Configuration();
+    }
+
     public Long rowsCount() {
-        return 6L;
+        return configuration
+                .getLongProperty(
+                        "machine.size.rows",
+                        6L);
     }
 
     public Long colsCount() {
-        return 4L;
+        return configuration
+                .getLongProperty(
+                        "machine.size.cols",
+                        4L
+                );
     }
 }
