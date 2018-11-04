8fe09f522a60020b08be1698b3962530579efd65
lachim kabucz <czubakmichal@gmail.com>
2018-11-04 10:18:57 +0100

    Commit add tray

--- src/main/java/pl/sdacademy/vending/model/VendingMachine.java
+++ src/main/java/pl/sdacademy/vending/model/VendingMachine.java
@@ -3,6 +3,7 @@ package pl.sdacademy.vending.model;
 import pl.sdacademy.vending.util.Configuration;
 
 import java.util.Optional;
+import java.util.Random;
 
 public class VendingMachine {
 
@@ -31,15 +32,72 @@ public class VendingMachine {
         }
         trays = new Tray[rowsCount.intValue()][colsCount.intValue()];
 
+
+        Random random = new Random();
         for (int rowNo = 0; rowNo < rowsCount; rowNo++) {
             for (int colNo = 0; colNo < colsCount; colNo++) {
-                char letter = (char) ('A' + rowNo);
-                int number = colNo + 1;
-                String symbol = "" + letter + number;
-                Tray tray = Tray.builder(symbol).build();
-                trays[rowNo][colNo] = tray;
+
+                // 0 1 2 3 4 5 6 7 8 9
+                if(random.nextInt(10) <8){
+
+                //if (Math.random() < 0.8) { //propability 0.8
+
+                    generateTrayAtPosition(rowNo, colNo);
+                }
             }
         }
+    }
+
+    private void generateTrayAtPosition(int rowNo, int colNo) {
+        Random random = new Random();
+
+        long price = random.nextInt(901) + 100;
+
+        char letter = (char) ('A' + rowNo);
+        int number = colNo + 1;
+        String symbol = "" + letter + number;
+        int productProbability = random.nextInt(10);
+        if(productProbability < 1 ){
+            //2produkty
+            Tray tray = Tray
+                    .builder(symbol)
+                    .price(price)
+                    .product(new Product("Product"+symbol))
+                    .product(new Product("Product"+symbol))
+                    .build();
+            trays[rowNo][colNo] = tray;
+
+        }
+        else if (productProbability < 5) {
+            //1product
+
+            Tray tray = Tray
+                    .builder(symbol)
+                    .price(price)
+                    .product(new Product("Product"+symbol))
+                    .build();
+            trays[rowNo][colNo] = tray;
+
+
+        }
+        else  {
+
+            Tray tray = Tray
+                    .builder(symbol)
+                    .price(price)
+                    .build();
+            trays[rowNo][colNo] = tray;
+
+        }
+
+//        Tray tray = Tray
+//                .builder(symbol)
+//                .price(price)
+//                .build();
+//        //0.1 -> 2 product
+//        //0.5 -> 1 product
+//
+//        trays[rowNo][colNo] = tray;
     }
 
     public Optional<Tray> getTrayAtPosition(int rowNo, int colNo) {
